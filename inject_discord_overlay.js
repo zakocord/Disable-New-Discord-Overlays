6314ca6607d61c2e966f8472346e7d983aebd5d7
zakocord <beehive_dev@outlook.jp>
2025-05-08 12:07:33 +0900

    Inject

---
+++ inject_discord_overlay.js
@@ -0,0 +1,14 @@
+console.log("injected E-Cord")
+   console.log(`
+███████╗     ██████╗ ██████╗ ██████╗ ██████╗ 
+██╔════╝    ██╔════╝██╔═══██╗██╔══██╗██╔══██╗
+█████╗█████╗██║     ██║   ██║██████╔╝██║  ██║
+██╔══╝╚════╝██║     ██║   ██║██╔══██╗██║  ██║
+███████╗    ╚██████╗╚██████╔╝██║  ██║██████╔╝
+╚══════╝     ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝ 
+   `);
+
+console.log ("Disabled | New Game Overlay")
+console.warn("Made By Zakocord");
+console.warn("Src-v1.0 | github/Zakocord")
+console.warn("Installer Version: Src-v1.0")
